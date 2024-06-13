92294d604b59d05a5ce9a718a5692577a8d40c69
kaduvuotto <kaduvuotto@hotmail.com>
2024-06-13 17:59:48 -0300

    fix: adjust search bar component

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,5 +1,5 @@
 import * as Styled from "./styles";
-import { SearchBar } from "./components/Searchbar";
+import { SearchBar } from "./components/SearchBar/SearchBar";
 import { Columns } from "./components/Columns/Columns";
  
  export const Dashboard = () => {
