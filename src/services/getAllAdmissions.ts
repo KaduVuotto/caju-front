cbe234097a8af33ac3560431bc83f769935fc2ec
kaduvuotto <kaduvuotto@hotmail.com>
2024-06-15 18:11:58 -0300

    fix: update get all admissions

--- src/services/getAllAdmissions.ts
+++ src/services/getAllAdmissions.ts
@@ -1,10 +1,11 @@
 import { api } from "./axios";
 
 export const getAllAdmissions = async () => {
-    try {
-      const response = await api.get('registrations');
-      return response.data;
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  try {
+    const response = await api.get("registrations");
+    return response.data;
+  } catch (error) {
+    console.error("Error get all admissions:", error);
+    throw error;
+  }
+};
