157cf02412eecb3322f2cd637380d3d3d4114a2f
kaduvuotto <kaduvuotto@hotmail.com>
2024-06-17 14:46:11 -0300

    feat: create initial state

---
+++ src/utils/initialStateRegistration.ts
@@ -0,0 +1,52 @@
+import { DataRegistrationsItem } from "~/types/interface";
+
+export const initialStateRegistration: DataRegistrationsItem[] = [
+    {
+      admissionDate: "00/00/0000",
+      email: "huiahdaiudhasuid",
+      employeeName: "",
+      status: "REVIEW",
+      cpf: "3947928374238",
+      id: "0",
+    },
+    {
+      admissionDate: "00/00/0000",
+      email: "",
+      employeeName: "",
+      status: "APPROVED",
+      cpf: "",
+      id: "1",
+    },
+    {
+      admissionDate: "00/00/0000",
+      email: "",
+      employeeName: "",
+      status: "REPROVED",
+      cpf: "",
+      id: "2",
+    },
+    {
+      admissionDate: "00/00/0000",
+      email: "",
+      employeeName: "",
+      status: "REVIEW",
+      cpf: "",
+      id: "3",
+    },
+    {
+      admissionDate: "00/00/0000",
+      email: "",
+      employeeName: "",
+      status: "APPROVED",
+      cpf: "",
+      id: "4",
+    },
+    {
+      admissionDate: "00/00/0000",
+      email: "",
+      employeeName: "",
+      status: "REPROVED",
+      cpf: "",
+      id: "5",
+    },
+  ];
