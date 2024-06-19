dc3ee71540e4fa0b345627810c05604305108963
kaduvuotto <kaduvuotto@hotmail.com>
2024-06-18 22:47:00 -0300

    fix: update validation schema

--- src/utils/validationSchemaNewAdmission.ts
+++ src/utils/validationSchemaNewAdmission.ts
@@ -4,7 +4,11 @@ import { validateCpf } from "./validateCpf";
 export const validationSchemaNewAdmission = Yup.object().shape({
   name: Yup.string()
     .required("Nome é obrigatório")
-    .min(2, "Nome deve ter pelo menos 3 caracteres"),
+    .min(2, "Nome deve ter pelo menos 3 caracteres")
+    .matches(
+      /^(?![\d])[A-Za-zÀ-ÿ ']{2,}(?: [A-Za-zÀ-ÿ ']+)+$/,
+      "Nome deve conter pelo menos um espaço, no mínimo duas letras e a primeira letra não pode ser um número!"
+    ),
   email: Yup.string().email("Email inválido").required("Email é obrigatório"),
   cpf: Yup.string()
     .required("CPF é obrigatório")
